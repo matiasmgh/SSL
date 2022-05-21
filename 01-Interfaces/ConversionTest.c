be1d816b72272a676d3de393790c6d6c59b1b1e4
matiasmgh <matiasmgh@gmail.com>
2022-05-21 20:06:46 -0300

    TP N°1 - Refactor tests para mayor legibilidad

--- 01-Interfaces/ConversionTest.c
+++ 01-Interfaces/ConversionTest.c
@@ -23,15 +23,19 @@
  * @return true Si la diferencia entre los dos valores es menor o igual al epsilon.
  * @return false Si la diferencia entre los dos valores es mayor al epsilon.
  */
-bool areEqual(double a, double b, double epsilon) {
-    bool returnValue;
+int areEqual(double a, double b, double epsilon) {
+    int returnValue;
     double diferencia = fabs(a - b);
     if(diferencia <= epsilon) {
-        returnValue = true;
+        returnValue = 1;
     } else {
-        returnValue = false;
+        returnValue = 0;
     }
     return returnValue;
+}
+
+void assertEqualFloatValue(double valorCalculado, double valorEsperado, double epsilon) {
+    assert(areEqual(valorCalculado, valorEsperado, epsilon));
 }
 
 void printSuccessfulMessage(double valorCalculado) {
@@ -46,27 +50,21 @@ void testCelsiusToFahrenheit() {
     printf("TESTS DE CONVERSION DE Celsius A Fahrenheit: \n");
     //Epsilon de referencia.
     const double DIFERENCIA_ACEPTABLE = 0.0002;
-    double valorEsperado;
 
     //Valores de prueba.
     const double CERO_CELSIUS = 0.0;
     const double CERO_CELSIUS_A_FAHR = fahrenheit(CERO_CELSIUS);
-    valorEsperado = 32.0;
-
-    //Tests.
-    assert(areEqual(CERO_CELSIUS_A_FAHR, valorEsperado, DIFERENCIA_ACEPTABLE));
-    printSuccessfulMessage(CERO_CELSIUS);
-
     const double MENOS_CINCUENTA_CELSIUS = -50.0;
     const double MENOS_CINCUENTA_CELSIUS_A_FAHR = fahrenheit(MENOS_CINCUENTA_CELSIUS);
-    valorEsperado = -58.0;
-    assert(areEqual(MENOS_CINCUENTA_CELSIUS_A_FAHR, valorEsperado, DIFERENCIA_ACEPTABLE));
-    printSuccessfulMessage(MENOS_CINCUENTA_CELSIUS);
-
     const double TRESCIENTOS_CELSIUS = 300.0;
     const double TRESCIENTOS_CELSIUS_A_FAHR = fahrenheit(TRESCIENTOS_CELSIUS);
-    valorEsperado = 572.0;
-    assert(areEqual(TRESCIENTOS_CELSIUS_A_FAHR, valorEsperado, DIFERENCIA_ACEPTABLE));
+
+    //Tests.
+    assertEqualFloatValue(CERO_CELSIUS_A_FAHR, 32.0, DIFERENCIA_ACEPTABLE);
+    printSuccessfulMessage(CERO_CELSIUS);
+    assertEqualFloatValue(MENOS_CINCUENTA_CELSIUS_A_FAHR, -58.0, DIFERENCIA_ACEPTABLE);
+    printSuccessfulMessage(MENOS_CINCUENTA_CELSIUS);
+    assertEqualFloatValue(TRESCIENTOS_CELSIUS_A_FAHR, 572.0, DIFERENCIA_ACEPTABLE);
     printSuccessfulMessage(TRESCIENTOS_CELSIUS);
 }
 
@@ -79,27 +77,21 @@ void testFahrenheitToCelsius() {
     
     //Epsilon de referencia.
     const double DIFERENCIA_ACEPTABLE = 0.0002;
-    double valorEsperado;
 
     //Valores de prueba.
     const double CERO_FAHR = 0.0;
     const double CERO_FAHR_A_CELSIUS = celsius(CERO_FAHR);
-    valorEsperado = -17.7778;
-
-    //Tests.
-    assert(areEqual(CERO_FAHR_A_CELSIUS, valorEsperado, DIFERENCIA_ACEPTABLE));
-    printSuccessfulMessage(CERO_FAHR);
-
     const double MENOS_CINCUENTA_FAHR = -50.0;
     const double MENOS_CINCUENTA_FAHR_A_CELSIUS = celsius(MENOS_CINCUENTA_FAHR);
-    valorEsperado = -45.5556;
-    assert(areEqual(MENOS_CINCUENTA_FAHR_A_CELSIUS, valorEsperado, DIFERENCIA_ACEPTABLE));
-    printSuccessfulMessage(MENOS_CINCUENTA_FAHR);
-
     const double TRESCIENTOS_FAHR = 300.0;
     const double TRESCIENTOS_FAHR_A_CELSIUS = celsius(TRESCIENTOS_FAHR);
-    valorEsperado = 148.889;
-    assert(areEqual(TRESCIENTOS_FAHR_A_CELSIUS, valorEsperado, DIFERENCIA_ACEPTABLE));
+    
+    //Tests.
+    assertEqualFloatValue(CERO_FAHR_A_CELSIUS, -17.7778, DIFERENCIA_ACEPTABLE);
+    printSuccessfulMessage(CERO_FAHR);
+    assertEqualFloatValue(MENOS_CINCUENTA_FAHR_A_CELSIUS, -45.5556, DIFERENCIA_ACEPTABLE);
+    printSuccessfulMessage(MENOS_CINCUENTA_FAHR);
+    assertEqualFloatValue(TRESCIENTOS_FAHR_A_CELSIUS, 148.889, DIFERENCIA_ACEPTABLE);
     printSuccessfulMessage(TRESCIENTOS_FAHR);
 }
 
